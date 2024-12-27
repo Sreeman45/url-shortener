810242e128709e2a2e28c37dc166a4701555c3d5
sreeman <sreemanlingala333@gmail.com>
2024-12-27 22:07:08 +0530

    removved a func

--- connection.js
+++ connection.js
@@ -6,7 +6,6 @@ dotenv.config();
 async function connectdb() {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
             useUnifiedTopology: true,    
         });
         console.log("Database connected successfully");
