f6a51b9d01a53ecc50e976b2eb82cdbf89e929eb
sreeman <sreemanlingala333@gmail.com>
2024-12-27 17:21:08 +0530

    added feature

--- connection.js
+++ connection.js
@@ -2,6 +2,6 @@ import {mongoose} from 'mongoose'
 import dotenv from 'dotenv';
 dotenv.config();
 async function connectdb() {
-    await mongoose.connect(`${process.env.MONGO}`).then(()=>console.log("database connected"))
+    await mongoose.connect(`${process.env.MONGO_URI}`).then(()=>console.log("database connected"))
 }
 export{connectdb}
